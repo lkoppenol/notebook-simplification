ba25d04afb4ef6ef124ed99aca99b618f64cba3e
Laulau <lt.koppenol@portofrotterdam.com>
2021-10-14 13:01:44 +0200

    first functions

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # notebook-simplification
 Complexity hiding of notebook utility functions
+
+
+## Usage
+```python
+!pip install git+https://github.com/lkoppenol/notebook-simplification
+
+from notebook_simplification.nbutils import *
+
+help()
+```
--- src/notebook_simplification/nbutils.py
+++ src/notebook_simplification/nbutils.py
@@ -1,3 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
-def test(a):
-    print(a)
+
+def discretize_predictions(predictions):
+    return np.argmax(predictions, axis=1)
+
+
+def plot_random_image(images, labels=None, predictions=None):
+    n = np.random.randint(0, len(images))
+    plot_specific_image(n, images, labels, predictions)
+
+
+def plot_specific_image(n, images, labels=None, predictions=None):
+    plt.figure()
+    plt.imshow(images[n])
+    title = f"Image id: {n}"
+    if labels:
+        title += f" - Truth: {labels[n]}"
+    if predictions:
+        title += f" - Truth: {predictions[n]}"
+    plt.title(title)
+
+
+def explain(variable, name=""):
+    print(f"Explanation of variable {name}")
+    print("===============================")
+    print(f"The type of this variable is {type(variable)}")
+    shaped = True
+    try:
+        print(f"The dimensions of this variable are {variable.shape}")
+    except AttributeError:
+        try:
+            print(f"The length of this variable is '{len(variable)}'")
+        except TypeError:
+            print(f"The dimensions of this variable are unknown, meaning it is either 0 dimensional or complex")
+            shaped = False
+    if shaped:
+        sample = str(variable[0])
+    else:
+        sample = str(variable)
+    if len(sample) > 77:
+        sample = sample[:77] + "..."
+    print("Sample of the data:")
+    print(sample)
+
+
+def help():
+    """
+    This function explains the current module.
+
+    :example:
+    help()
+    """
+    print(
+        "Welcome!\n"
+        "This library contains various functions to help you keep your notebooks straightforward. It has been developed"
+        " for a specific training and functions might not be as generic as you like. Below is a list of helper"
+        "functions available:"
+    )
+    for func in globals():
+        if not str(func).startswith("__"):
+            print(f" - {func}")
+    print("\n\nFor more information run `function_name?` in seperate cell. For example `help?`")
